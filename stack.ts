63dba3244a697db88eb93a5b3d372e8029e4b1cc
jaeyoung.choi <lactrious@gmail.com>
2020-01-26 21:14:13 +0900

    Add authorizer

--- stack.ts
+++ stack.ts
@@ -4,40 +4,75 @@ import cdk = require("@aws-cdk/core");
 import * as path from "path";
 import build from "./build";
 
-interface IApiHandler {
-  apiName: string;
+interface ILambdaHandler {
+  name: string;
   sourcePath: string;
-  handlerName?: string;
+  functionName?: string;
+}
+
+interface IApiHandler extends ILambdaHandler {
   apiPath?: string;
   method: "GET" | "POST" | "DELETE" | "PUT";
 }
 
-type ApiHandler = Omit<IApiHandler, "sourcePath"> & { code: string };
+type CompiledHandler<T extends { sourcePath: string }> = Omit<
+  T,
+  "sourcePath"
+> & { code: string };
 
 export class ApiLambdaHelloWorldStack extends cdk.Stack {
-  constructor(app: cdk.App, id: string, handlers: ApiHandler[]) {
+  constructor(
+    app: cdk.App,
+    id: string,
+    handlers: Array<CompiledHandler<IApiHandler>>,
+    authorizerHandler?: CompiledHandler<ILambdaHandler>
+  ) {
     super(app, id);
+
+    const authorizer = authorizerHandler
+      ? this.buildAuthorizer(app, authorizerHandler)
+      : undefined;
+    const authOptions: Partial<apigateway.MethodOptions> = authorizer
+      ? { authorizer, authorizationType: apigateway.AuthorizationType.CUSTOM }
+      : {};
 
     for (const handler of handlers) {
       const handlerLambda = new lambda.Function(
         this,
-        handler.apiName + "Function",
+        handler.name + "Function",
         {
           code: new lambda.InlineCode(handler.code),
-          handler: "index." + (handler.handlerName || "handle"),
+          handler: "index." + (handler.functionName || "handle"),
           runtime: lambda.Runtime.NODEJS_12_X
         }
       );
 
-      const api = new apigateway.RestApi(this, handler.apiName + "Api");
+      const api = new apigateway.RestApi(this, handler.name + "Api");
       const resource = handler.apiPath
         ? api.root.addResource(handler.apiPath)
         : api.root;
       addCorsOptions(resource).addMethod(
         handler.method,
-        new apigateway.LambdaIntegration(handlerLambda)
+        new apigateway.LambdaIntegration(handlerLambda, {}),
+        {
+          ...authOptions
+        }
       );
     }
+  }
+
+  private buildAuthorizer(
+    app: cdk.App,
+    { name, code, functionName }: CompiledHandler<ILambdaHandler>
+  ) {
+    const handlerLambda = new lambda.Function(this, name + "Function", {
+      code: new lambda.InlineCode(code),
+      handler: "index." + (functionName || "handle"),
+      runtime: lambda.Runtime.NODEJS_12_X
+    });
+    return new apigateway.TokenAuthorizer(app, name + "Authorizer", {
+      handler: handlerLambda
+    });
   }
 }
 
@@ -81,8 +116,8 @@ function addCorsOptions(apiResource: apigateway.IResource) {
   return apiResource;
 }
 
-async function loadSources(input: IApiHandler[]): Promise<ApiHandler[]> {
-  const handlers: ApiHandler[] = [];
+async function loadSources(input: IApiHandler[]) {
+  const handlers: Array<CompiledHandler<IApiHandler>> = [];
   for (const each of input) {
     const code = await build(path.join(__dirname, each.sourcePath));
     handlers.push({
@@ -97,10 +132,10 @@ async function loadSources(input: IApiHandler[]): Promise<ApiHandler[]> {
   try {
     const handlers = await loadSources([
       {
-        apiName: "helloWorld",
+        name: "helloWorld",
         method: "GET",
         sourcePath: "src/hello.ts",
-        handlerName: "handle"
+        functionName: "handle"
       }
     ]);
     const app = new cdk.App();
